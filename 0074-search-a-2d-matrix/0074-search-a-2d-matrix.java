d3afc11326b715f180f4bc03a985c3b7749f0062
kanankotwani28 <kanankotwani28@gmail.com>
2025-06-17 23:42:34 +0530

    Time: 0 ms (100%), Space: 42.5 MB (27.83%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int left = 0, right = m * n - 1;
+
+        while (left <= right) {
+            int mid = (left + right) >>> 1;  // Prevents overflow
+            int row = mid / n;
+            int col = mid % n;
+            int num = matrix[row][col];
+
+            if (num == target) {
+                return true;
+            } else if (num < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
